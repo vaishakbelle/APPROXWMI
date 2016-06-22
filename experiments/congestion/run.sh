24960b7f0a6765a24fb504359778555063ba54c4
Andrea Passerini <passerini@disi.unitn.it>
2016-06-22 17:14:50 +0200

    added stats computation for experiments

--- experiments/congestion/run.sh
+++ experiments/congestion/run.sh
@@ -3,6 +3,9 @@ lengths="6 7 8"
 for l in $lengths
 do
   echo "Problem length: $l"
-  python run.py "data/Sep 2014.csv" 3 random $l 6 > results/A6_${l}_6.log
+  problem=A6_${l}_6
+  python run.py "data/Sep 2014.csv" 3 random $l 6 > results/$problem.log
+  cat results/$problem.log | grep -e EXACT -e APPROX -A 5 > results/$problem.stats
+  python results/volume_computations.py results/$problem.log > results/$problem.vol
 done
 
