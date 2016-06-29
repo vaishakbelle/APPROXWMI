9b81eb5a123c6aa0e3e9bbb36ecf99d58e71966d
Andrea Passerini <passerini@lion0B.science.unitn.it>
2016-06-29 12:16:08 +0200

    added volume diff computation

--- experiments/congestion/run.sh
+++ experiments/congestion/run.sh
@@ -7,5 +7,6 @@ do
   problem=A6_${l}_6
   python run.py "data/Sep 2014.csv" 3 random $tilt $l 6 > results/$problem.log
   cat results/$problem.log | grep -e EXACT -e APPROX -A 5 > results/$problem.stats
+  cat results/$problem.log | grep difference > results/$problem.diff
   python results/volume_computations.py results/$problem.log > results/$problem.vol
 done
